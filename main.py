d9137bb3c576ae3cadb0d07a7a9b00c0ad7f1f1e
Markiyan Pyts <markiyan.pyts@osf.digital>
2025-06-05 14:38:07 +0300

    fix: update import statement and correct environment variable usage

--- main.py
+++ main.py
@@ -5,14 +5,15 @@ from dotenv import load_dotenv
 load_dotenv()
 import os
 
-from langchain_mcp_adapters import load_mcp_tools
+from langchain_mcp_adapters.tools import load_mcp_tools
 from langchain_openai import ChatOpenAI
 from langgraph.prebuilt import create_react_agent
 from mcp import ClientSession, StdioServerParameters
 from mcp.client.stdio import stdio_client
 
-print("OPENAI_API_KEY:", os.getenv("OPENAI_API_KEY"))
-
+print("OPENAI_API_KEY:", os.getenv("LANGSMITH_ENDPOINT"))
+llm = ChatOpenAI()
+stdio_server_params = StdioServerParameters(command="python", args=["/Users/markiyanpyts/Documents/GitHub/mcp-langchain/servers/math_server.py"])
 async def main():
     print("Hello from mcp-langchain! ")
 
