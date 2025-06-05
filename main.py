aeab6edc454907479eae8fe9316d5414a0e0a712
Markiyan Pyts <markiyan.pyts@osf.digital>
2025-06-05 14:28:02 +0300

    feat: add isort dependency and update related files

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import asyncio
+
 from dotenv import load_dotenv
+
 load_dotenv()
 import os
+
+from langchain_mcp_adapters import load_mcp_tools
+from langchain_openai import ChatOpenAI
+from langgraph.prebuilt import create_react_agent
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
+
 print("OPENAI_API_KEY:", os.getenv("OPENAI_API_KEY"))
 
 async def main():
