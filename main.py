e885c680459e3e630eee0a25235f957ffdb6eeb2
Markiyan Pyts <markiyan.pyts@osf.digital>
2025-06-05 21:03:34 +0300

    feat: enhance main.py with async session handling and weather query functionality

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 import os
+from langchain_core.messages import HumanMessage
 
 from langchain_mcp_adapters.tools import load_mcp_tools
 from langchain_openai import ChatOpenAI
@@ -11,11 +12,25 @@ from langgraph.prebuilt import create_react_agent
 from mcp import ClientSession, StdioServerParameters
 from mcp.client.stdio import stdio_client
 
-print("OPENAI_API_KEY:", os.getenv("LANGSMITH_ENDPOINT"))
 llm = ChatOpenAI()
 stdio_server_params = StdioServerParameters(command="python", args=["/Users/markiyanpyts/Documents/GitHub/mcp-langchain/servers/math_server.py"])
 async def main():
     print("Hello from mcp-langchain! ")
+    async with stdio_client(stdio_server_params) as (read, write):
+        async with ClientSession(read_stream=read, write_stream=write) as session:
+            await session.initialize()
+            print("Session initialized")
+            tools = await load_mcp_tools(session)
+            print("Tools:", tools)
+            agent = create_react_agent(llm, tools)
+
+            result = await agent.invoke(
+                HumanMessage(
+                    content="What is the sum of 2 and 3?"
+                )
+            )
+            print("Result:", result.messages[-1].content)
+     
 
 
 if __name__ == "__main__":
