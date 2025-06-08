b0dfca4e38901598ece79fed6ae8036976bd4b65
Markiyan Pyts <markiyan.pyts@osf.digital>
2025-06-08 19:09:52 +0300

    work

--- langchain_client.py
+++ langchain_client.py
@@ -11,28 +11,27 @@ llm = ChatOpenAI(api_key=os.getenv("OPENAI_API_KEY2"))
 
 
 async def main():
-    async with MultiServerMCPClient(
-        {
-            "math": {
-                "command": "python",
-                "args": [
-                    "/Users/markiyanpyts/Documents/GitHub/mcp-langchain/servers/math_server.py"
-                ],
-            },
-            "weather": {
-                "url": "http://127.0.0.1:8000",
-                "transport": "sse",
-            },
+    client = MultiServerMCPClient(
+    {
+        "math": {
+            "command": "python",
+            # Make sure to update to the full absolute path to your math_server.py file
+            "args": ["/Users/markiyanpyts/Documents/GitHub/mcp-langchain/servers/math_server.py"],
+            "transport": "stdio",
+        },
+        "weather": {
+            # make sure you start your weather server on port 8000
+            "url": "http://localhost:8000/mcp",
+            "transport": "streamable-http",
         }
-    ) as client:
-        agent = create_react_agent(llm, client.get_tools())
-        result = await agent.ainvoke(
-            {
-                "messages": "What is the sum of 54 + 10 * 2? "
-            }
-        )
-        print("Result:", result["messages"][-1].content)
-
+    }
+    )
+    tools = await client.get_tools()
+    agent = create_react_agent("openai:gpt-4.1", tools)
+    math_response = await agent.ainvoke({"messages": "what's (3 + 5) x 12?"})
+    weather_response = await agent.ainvoke({"messages": "what is the weather in nyc?"})
+    print("Math Response:", math_response["messages"][-1].content)
+    print("Weather Response:", weather_response["messages"][-1].content)
 
 if __name__ == "__main__":
     asyncio.run(main())
